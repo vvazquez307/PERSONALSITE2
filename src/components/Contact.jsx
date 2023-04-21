c8ac118dd251dda3756006218d0b7d91dcfd19ae
victor <vvazquez307@gmail.com>
2023-04-21 12:17:49 -0500

    projects page edited

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,41 +9,43 @@ const Contact = () => {
   }
   return (
     <div id="contact-content">
-      <div id="contact-title">
-        <h1>Contact Information</h1>
+      <div id="contact-box">
+        <div className="h2-center">
+          <h1>Contact Information</h1>
+        </div>
+        <div id="contact-info">
+          <h2 className="h2-center">Email: Vvazquez307.w@gmail.com</h2>
+          <h2 className="h2-center">Phone Number: 773-396-6632</h2>
+          <h2 className="h2-center">
+            LinkedIn Profile: {` `}
+            <Link
+              className="text-link"
+              to="https://www.linkedin.com/in/victor-vazquez-971268272/"
+              target="_blank"
+            >
+              Click Here!
+            </Link>
+          </h2>
+          <h2 className="h2-center">
+            Github Profile: {` `}
+            <Link
+              className="text-link"
+              to="https://github.com/vvazquez307"
+              target="_blank"
+            >
+              Click Here!
+            </Link>
+          </h2>
+        </div>
+        <button
+          id="contact-back-btn"
+          onClick={() => {
+            navigateHome();
+          }}
+        >
+          Go to Home
+        </button>
       </div>
-      <div id="contact-info">
-        <h2>Email: Vvazquez307.w@gmail.com</h2>
-        <h2>Phone Number: 773-396-6632</h2>
-        <h2>
-          LinkedIn Profile: {` `}
-          <Link
-            className="text-link"
-            to="https://www.linkedin.com/in/victor-vazquez-971268272/"
-            target="_blank"
-          >
-            Click Here!
-          </Link>
-        </h2>
-        <h2>
-          Github Profile: {` `}
-          <Link
-            className="text-link"
-            to="https://github.com/vvazquez307"
-            target="_blank"
-          >
-            Click Here!
-          </Link>
-        </h2>
-      </div>
-      <button
-        id="contact-back-btn"
-        onClick={() => {
-          navigateHome();
-        }}
-      >
-        Go to Home
-      </button>
     </div>
   );
 };
